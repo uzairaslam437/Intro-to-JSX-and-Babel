49cc6d38daa38035e5a87cbae6dd33f13f64797f
Uzair Aslam <118652354+uzairaslam437@users.noreply.github.com>
2023-08-11 11:38:08 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>hello!!</h1>
+    <p>This is our World.</p>
+  </div>,
+  document.getElementById("root")
+);
